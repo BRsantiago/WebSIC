2388ab12be6838e4944da331da4f44363916defd
BRsantiago <43882153+BRsantiago@users.noreply.github.com>
2018-10-23 10:49:59 -0300

    Add migrations Curso sem turma

---
+++ Entity/Entity/CursoSemTurma.cs
@@ -0,0 +1,29 @@
+﻿using Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Entity
+{
+    class CursoSemTurma : Base
+    {
+        public CursoSemTurma() : base()
+        {
+        }
+
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int IdCursoSemTurma { get; set; }
+        
+        [Display(Name = "Data de Validade")]
+        public DateTime DataValidade { get; set; }
+
+        public List<Curso> Cursos { get; set; }
+        public Pessoa Pessoa { get; set; }
+
+}
+}
