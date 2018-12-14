3700fa79703a5d7197a67f86d59fa07a554c0ac8
BRsantiago <43882153+BRsantiago@users.noreply.github.com>
2018-12-13 22:14:04 -0300

    Ajustes no menu dos relatórios

--- WebSIC/Controllers/RelatorioController.cs
+++ WebSIC/Controllers/RelatorioController.cs
@@ -46,6 +46,66 @@ namespace WebSIC.Controllers
             return View(model);
         }
 
+        public ActionResult CobrancaFiltro()
+        {
+            RelatorioViewModel model = new RelatorioViewModel();
+
+            model.Aeroportos = this.aeroportoService.ObterTodos();
+            model.Empresas = new List<Empresa>();
+
+            return View(model);
+        }
+
+        public ActionResult FuncionariosPorEmpresaFiltro()
+        {
+            RelatorioViewModel model = new RelatorioViewModel();
+
+            model.Aeroportos = this.aeroportoService.ObterTodos();
+            model.Empresas = new List<Empresa>();
+
+            return View(model);
+        }
+
+        public ActionResult TermoCancelamentoFiltro()
+        {
+            RelatorioViewModel model = new RelatorioViewModel();
+
+            model.Aeroportos = this.aeroportoService.ObterTodos();
+            model.Empresas = new List<Empresa>();
+
+            return View(model);
+        }
+
+        public ActionResult TermoDestruicaoFiltro()
+        {
+            RelatorioViewModel model = new RelatorioViewModel();
+
+            model.Aeroportos = this.aeroportoService.ObterTodos();
+            model.Empresas = new List<Empresa>();
+
+            return View(model);
+        }
+
+        public ActionResult TermoIndeferimentoFiltro()
+        {
+            RelatorioViewModel model = new RelatorioViewModel();
+
+            model.Aeroportos = this.aeroportoService.ObterTodos();
+            model.Empresas = new List<Empresa>();
+
+            return View(model);
+        }
+
+        public ActionResult TermoViaAdicionalFiltro()
+        {
+            RelatorioViewModel model = new RelatorioViewModel();
+
+            model.Aeroportos = this.aeroportoService.ObterTodos();
+            model.Empresas = new List<Empresa>();
+
+            return View(model);
+        }
+
         public ActionResult RenderizarRelatorioCredenciaisEmitidas(RelatorioViewModel model)
         {
             var reportViewer = new ReportViewer()
@@ -58,6 +118,144 @@ namespace WebSIC.Controllers
             reportViewer.ShowCredentialPrompts = false;
 
             reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Credenciais Emitidas";
+            reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
+
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAFINAL", model.DataFinal));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("IDEMPRESA", model.IdEmpresa.ToString()));
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return PartialView("../Shared/Report");
+        }
+
+        public ActionResult RenderizarRelatorioCobranca(RelatorioViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true
+            };
+
+            reportViewer.ShowParameterPrompts = false;
+            reportViewer.ShowCredentialPrompts = false;
+
+            reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Cobrança";
+            reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
+
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAFINAL", model.DataFinal));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("IDEMPRESA", model.IdEmpresa.ToString()));
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return PartialView("../Shared/Report");
+        }
+
+        public ActionResult RenderizarRelatorioFuncionariosPorEmpresa(RelatorioViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true
+            };
+
+            reportViewer.ShowParameterPrompts = false;
+            reportViewer.ShowCredentialPrompts = false;
+
+            reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Funcionários Por Empresa";
+            reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
+
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAFINAL", model.DataFinal));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("IDEMPRESA", model.IdEmpresa.ToString()));
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return PartialView("../Shared/Report");
+        }
+
+        public ActionResult RenderizarRelatorioTermoCancelamento(RelatorioViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true
+            };
+
+            reportViewer.ShowParameterPrompts = false;
+            reportViewer.ShowCredentialPrompts = false;
+
+            reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Termo de Cancelamento";
+            reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
+
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAFINAL", model.DataFinal));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("IDEMPRESA", model.IdEmpresa.ToString()));
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return PartialView("../Shared/Report");
+        }
+
+        public ActionResult RenderizarRelatorioTermoDestruicao(RelatorioViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true
+            };
+
+            reportViewer.ShowParameterPrompts = false;
+            reportViewer.ShowCredentialPrompts = false;
+
+            reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Termo de Destruição";
+            reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
+
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAFINAL", model.DataFinal));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("IDEMPRESA", model.IdEmpresa.ToString()));
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return PartialView("../Shared/Report");
+        }
+
+        public ActionResult RenderizarRelatorioTermoIndeferimento(RelatorioViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true
+            };
+
+            reportViewer.ShowParameterPrompts = false;
+            reportViewer.ShowCredentialPrompts = false;
+
+            reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Termo de Indeferimento";
+            reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
+
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("DATAFINAL", model.DataFinal));
+            reportViewer.ServerReport.SetParameters(new ReportParameter("IDEMPRESA", model.IdEmpresa.ToString()));
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return PartialView("../Shared/Report");
+        }
+
+        public ActionResult RenderizarRelatorioViaAdicional(RelatorioViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true
+            };
+
+            reportViewer.ShowParameterPrompts = false;
+            reportViewer.ShowCredentialPrompts = false;
+
+            reportViewer.ServerReport.ReportPath = ConfigurationManager.AppSettings["ReportServerPath"] + "Termo de via Adicional";
             reportViewer.ServerReport.ReportServerUrl = new Uri(ConfigurationManager.AppSettings["ReportServer"]);
 
             reportViewer.ServerReport.SetParameters(new ReportParameter("DATAINICIAL", model.DataInicial));
